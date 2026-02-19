31b15ac4478ff543ff9b82879819dbc9677e740d
crcistianbracho10 <cristianbracho904@gmail.com>
2026-02-19 13:47:42 -0400

    Update playerjs.js

--- playerjs.js
+++ playerjs.js
@@ -1,30 +1,13 @@
-// Asegúrate de tener Clappr cargado en tu página antes de este script
+// Asegúrate de tener Clappr cargado en tu página:
 // <script src="https://cdn.jsdelivr.net/npm/clappr@latest/dist/clappr.min.js"></script>
 
 (function() {
-  // URL base de la miniatura generada por tu servidor con FFmpeg
-  var posterBase = "https://tuservidor.com/thumb.jpg";
-
-  // Inicializa el reproductor
   var player = new Clappr.Player({
     source: "https://vs20.live.opencaster.com/cristianhilos_314b91b0/index.m3u8",
-    parentId: "#player", // el contenedor debe existir en tu HTML
+    parentId: "#player",
     autoPlay: true,
+    mute: false,
     width: "100%",
-    height: "100%",
-    poster: posterBase + "?t=" + Date.now()
+    height: "100%"
   });
-
-  // Función para refrescar la miniatura cada 20 segundos
-  function refreshPoster() {
-    var newPoster = posterBase + "?t=" + Date.now();
-    player.options.poster = newPoster;
-    var posterEl = player.core.$el.find('.poster-background');
-    if (posterEl.length) {
-      posterEl.css('background-image', 'url(' + newPoster + ')');
-    }
-  }
-
-  // Ejecuta la actualización cada 20 segundos
-  setInterval(refreshPoster, 20000);
 })();
